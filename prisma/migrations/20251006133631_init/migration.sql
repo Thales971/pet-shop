7bbed35acff5f74056dd75dfdd11db389d2f39a3
Thales971 <thales.t.silva6@aluno.senai.br>
2025-10-06 10:51:45 -0300

    first commit

---
+++ prisma/migrations/20251006133631_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "PetShops" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "especie" TEXT NOT NULL,
+    "idade" INTEGER NOT NULL,
+    "dono" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PetShops_pkey" PRIMARY KEY ("id")
+);
